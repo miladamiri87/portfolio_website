956768117e26ed5a0644932d49ac11901e3b278b
milad aimrir <miladamiri87@gmail.com>
2025-02-13 10:00:32 -0500

    change db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import team from '../assets/team.png'
 import photo_team from '../assets/photo_team.JPG'
+import projectImage from '../assets/kowsar/Enscape_2021-12-22-19-25-10-Photoroom.jpg'; // import کردن تصویر
+
 
 
 import EngineeringIcon from '@mui/icons-material/Engineering';
@@ -24,25 +26,96 @@ const Home = ({ scrollToServices }) => {
     };
 
 
-    const [projects, setProjects] = useState([]);
-    useEffect(() => {
-        console.log('در حال تلاش برای بارگذاری JSON...');
-        fetch("/projects.json")
-            .then((res) => {
-                console.log('پاسخ دریافت شد:', res);
-                if (!res.ok) {
-                    throw new Error('خطا در بارگذاری فایل JSON');
-                }
-                return res.json();
-            })
-            .then((data) => {
-                console.log('داده‌ها دریافت شدند:', data);
-                setProjects(data.projects_example);
-            })
-            .catch((error) => {
-                console.error('مشکلی در بارگذاری پروژه‌ها پیش آمد:', error);
-            });
-    }, []);
+    const [projects, setProjects] = useState([
+
+
+        {
+            "id": "1",
+            "type_project": "Design of a residential tower",
+            "name_project": "Kowsar Tower",
+            "role_project": "My role was to design the residential tower and oversee its execution and coordination.",
+            "description_project": "The project involves the design of a residential tower with a total floor area of 56,000 square meters. The tower was designed to provide a modern, functional living environment with attention to aesthetic details and user experience. The design focused on maximizing space efficiency while ensuring comfort for residents. The structure includes a mix of amenities, including recreational spaces, parking, and retail areas. Detailed architectural planning and coordination with structural, mechanical, and electrical systems were key to the project’s success.",
+            "project_intro_img": projectImage,
+            "year_project": 2022,
+            "location_project": "Mashhad, Iran",
+            "area_project": "56,000 sqm",
+            "project_detail_img": [
+                "/assets/kowsar/42-Photoroom.jpg",
+                "/assets/kowsar/43-Photoroom.jpg",
+                "/assets/kowsar/44-Photoroom.jpg",
+                "/assets/kowsar/45-Photoroom.jpg",
+                "/assets/kowsar/Enscape_2021-12-22-19-25-10-Photoroom.jpg",
+                "/assets/kowsar/Enscape_2021-12-22-19-31-13-Photoroom.jpg",
+                "/assets/kowsar/Enscape_2021-12-22-19-35-17-Photoroom.jpg",
+                "/assets/kowsar/Enscape_2021-12-22-19-42-20-Photoroom.jpg",
+                "/assets/kowsar/Enscape_2022-02-12-11-40-06.png",
+                "/assets/kowsar/24-12-99.mp4"
+            ]
+        },
+        {
+            "id": "2",
+            "type_project": "Amusement park design",
+            "name_project": "FUN ZONE",
+            "role_project": "Architect was to design the project and oversee its execution.",
+            "description_project": "The amusement park design is exposed, aligning with a VR and modern theme. The broken theme creates harmony between space and function. A suitable roof cover is achieved using metal boxes, and the lighting design is tailored to enhance the experience. The design is implemented using BIM, ensuring full coordination between architectural, mechanical, and lighting systems. This approach guarantees seamless integration of all elements, optimizing both aesthetic and functional aspects of the project.",
+            "project_intro_img": "/assets/funzone/intro.jpg",
+            "year_project": 2023,
+            "location_project": "Mashhad, Iran",
+            "area_project": "7,500 sqm",
+            "project_detail_img": [
+                "/assets/funzone/_MG_9646.JPG",
+                "/assets/funzone/_MG_9652.JPG",
+                "/assets/funzone/C0010S03.MP4",
+                "/assets/funzone/C0053S03.MP4",
+                "/assets/funzone/C0077S03.MP4",
+                "/assets/funzone/C0085S03.MP4",
+                "/assets/funzone/C0104S03.MP4",
+                "/assets/funzone/C0088S03.MP4",
+                "/assets/funzone/C0115S03.MP4",
+                "/assets/funzone/C0118S03.MP4",
+                "/assets/funzone/C0152S03.MP4",
+                "/assets/funzone/_MG_9666.JPG",
+                "/assets/funzone/_MG_9670.JPG",
+                "/assets/funzone/_MG_9676.JPG",
+                "/assets/funzone/_MG_9682.JPG",
+                "/assets/funzone/_MG_9693.JPG",
+                "/assets/funzone/_MG_9705.JPG",
+                "/assets/funzone/_MG_9717.JPG",
+                "/assets/funzone/_MG_9728.JPG",
+                "/assets/funzone/_MG_9733.JPG",
+                "/assets/funzone/_MG_9754.JPG",
+                "/assets/funzone/_MG_9764.JPG",
+                "/assets/funzone/_MG_9802.JPG",
+                "/assets/funzone/_MG_9812.JPG",
+                "/assets/funzone/_MG_9817.JPG",
+                "/assets/funzone/C0193S03.MP4"
+            ]
+        },
+        {
+            "id": "3",
+            "type_project": "Design Complex Residential",
+            "name_project": "Daneshjoo Complex",
+            "role_project": " My role was to design the residential complex and oversee its execution and coordination.",
+            "description_project": "The project involves the design of a residential complex for 1,000 people, focusing on optimizing space and functionality. Graph analysis was used to study gathering areas, utilizing Depthmap software to assess spatial configurations and movement patterns. The Justified Plan Graph algorithm was applied to enhance connectivity and accessibility within the complex. The design integrates modern planning principles to ensure efficient circulation, community engagement, and a well-balanced living environment while maximizing comfort and usability for residents.",
+            "project_intro_img": "/assets/nilofar/intro.jpg",
+            "year_project": 2017,
+            "location_project": "Tehran, Iran",
+            "area_project": "85,000 sqm",
+            "project_detail_img": [
+                "/assets/nilofar/1-1.png",
+                "/assets/nilofar/2-1.png",
+                "/assets/nilofar/3-1.png",
+                "/assets/nilofar/37.jpg",
+                "/assets/nilofar/38.jpg",
+                "/assets/nilofar/39.jpg",
+                "/assets/nilofar/40.jpg"
+            ]
+        }
+    ]
+
+
+    );
+
 
 
 
@@ -88,12 +161,8 @@ const Home = ({ scrollToServices }) => {
         {/* projects example */}
 
         <div id='projects_example_container'>
-
             {projects.map((project) => (
-
                 <div id='projects_example_content' key={project.id}>
-
-
                     <img src={project.project_intro_img} alt={project.name_project} />
                     <div id='projects_example_detail'>
                         <h3>{project.name_project}</h3>
