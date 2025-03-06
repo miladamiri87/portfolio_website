aee6d78269c67efd04ab260ef7cd39568a643562
milad aimrir <miladamiri87@gmail.com>
2025-03-06 17:34:48 -0500

    Resolve merge conflicts

---
+++ src/pages/footer.jsx
@@ -0,0 +1,45 @@
+
+import '../assets/Styles/footer.scss';
+
+import logo from '../assets/logo.png'
+
+const Footer = () => {
+    return (
+        <div id='footer_coppright'>
+
+            <div id='footer_content'>
+                <div id='footer_logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div id='footer_container'>
+                    <div id='footer_address'>
+                        <h3>Address</h3>
+                        <p>
+                            Thornhil,ON</p>
+                    </div>
+                    <div id='footer_phone'>
+                        <h3>Phone</h3>
+                        <p>(+1) 647-229-8463</p>
+                    </div>
+                    <div id='footer_email'>
+                        <h3>Email</h3>
+                        <p>forooghzaman68@gmail.com</p>
+                        <p>miladamiri87@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <p>All rights reserved &copy; 2025. This website is owned by Milad Amiri, Designer and Architect.</p>
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import '../assets/Styles/responsive-page-break_point.scss';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import logo from '../assets/logo.png'
+
+
+function Navbar({ scrollToServices }) {
+
+    const location = useLocation(); // دریافت مسیر فعلی
+    const navigate = useNavigate(); // برای هدایت به Home
+
+    const handleScrollToServices = (e) => {
+        e.preventDefault(); // جلوگیری از تغییر صفحه ناخواسته
+
+        if (location.pathname !== "/portfolio_website") {
+            // اگر در صفحه جزئیات هستیم، اول به Home برو و بعد اسکرول کن
+            navigate("/portfolio_website");
+            setTimeout(() => {
+                if (scrollToServices.current) {
+                    scrollToServices.current();
+                }
+            }, 100); // مقدار کم برای اطمینان از لود شدن صفحه
+        } else {
+            // اگر در Home هستیم، مستقیم اسکرول کند
+            if (scrollToServices.current) {
+                scrollToServices.current();
+            }
+        }
+    };
+
+
+    return (
+        <div id='header'>
+            <div id='contact_container'>
+                <div id='address'>
+                    <span ><i className='bx bx-map-pin'></i></span>
+                    <p>
+                        Thornhil,ON</p>
+                </div>
+                <div id="logo">
+                    <img src={logo} alt="logo" />
+
+                </div>
+                <div id='phone'>
+                    <span><i className='bx bxs-phone-call' ></i></span>
+                    <p>(+1) 647-229-8463</p>
+
+                </div>
+            </div>
+            <div id='menu'>
+                <Link to="/portfolio_website">Home</Link>
+                <a href="#" onClick={handleScrollToServices}>Services</a>
+                <Link to="/all_projects">All Projects</Link>
+                <Link to="/about">About</Link>
+
+            </div>
+
+        </div>
+    )
+}
+export default Navbar;
